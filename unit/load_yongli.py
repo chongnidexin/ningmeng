6aaff3752800370d2414be489ceb2aad5e1edba2
chongnidexin@yeah.net <chongnidexin@yeah.net>
2020-01-16 00:41:56 +0800

    加载用例

--- unit/load_yongli.py
+++ unit/load_yongli.py
@@ -1,4 +1,3 @@
-from danyuanceshi import TestMathMethod
 import unittest
 
 suite = unittest.TestSuite()
@@ -12,19 +11,16 @@ suite = unittest.TestSuite()
 
 
 # 方法二：
-import danyuanceshi
-import HtmlTestRunner
-# TestLoader  两种方法，一种根据模块名  ，一种针对类名
-# loader = unittest.TestLoader()  # 创建一个加载器
-# suite.addTest(loader.loadTestsFromTestCase(TestMathMethod))
-# suite.addTest(loader.loadTestsFromModule(danyuanceshi))
-# with open("test.txt", "w+", encoding="UTF-8") as file:
-#     runner = unittest.TextTestRunner(stream=file, verbosity=2)
-#     runner.run(suite)
 
-with open("test_report.html", 'wb') as file:
-    runner = HtmlTestRunner.HTMLTestRunner( stream=file, output="./reports/", verbosity=2, report_title="首次测试报告", descriptions=True, failfast=False, buffer=False,
-                  report_name=None, template=None, resultclass=None,
-                 add_timestamp=True, open_in_browser=False,
-                 combine_reports=False, template_args=None)
+# TestLoader  两种方法，一种根据模块名  ，一种针对类名
+import danyuanceshi
+loader = unittest.TestLoader()  # 创建一个加载器
+#suite.addTest(loader.loadTestsFromTestCase(TestMathMethod))
+suite.addTest(loader.loadTestsFromModule(danyuanceshi))
+with open("test.txt", "w+", encoding="UTF-8") as file:
+    runner = unittest.TextTestRunner(stream=file, verbosity=2)
     runner.run(suite)
+# from BeautifulReport   import BeautifulReport
+# with open("test_report.html", 'wb') as file:
+#     runner = BeautifulReport(suite)
+#     runner.run(suite)
